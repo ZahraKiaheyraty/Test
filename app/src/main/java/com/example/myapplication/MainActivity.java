f92d53c890ea3a8abbe54e402df28a9d1f53753a
work <zahra.kiaheyrati1@gmail.com>
2022-02-26 16:03:41 +0330

    UI

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        LottieAnimationView animationView = findViewById(R.id.animation_view);
+        animationView
+                .addAnimatorUpdateListener(
+                        (animation) -> {
+                        });
+        animationView
+                .playAnimation();
+
+        if (animationView.isAnimating()) {
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, Setting.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
