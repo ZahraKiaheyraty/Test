54849d3f358a411c575da2f9d66fbf902cfbe943
work <zahra.kiaheyrati1@gmail.com>
2022-03-06 10:26:24 +0330

    OK Random

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,48 +20,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
-    Timer timeoutTimer;
-    final Random myRandom = new Random();
-    GenerateTask genTask = new GenerateTask();
-    final ArrayList<ArrayList<Integer>> arry1 = new ArrayList<ArrayList<Integer>>();
-    final ArrayList<ArrayList<Double>> arry2 = new ArrayList<ArrayList<Double>>();
+    int count=0;
 
-    class GenerateTask extends TimerTask {
-        boolean started = false;
-        @Override
-        public void run() {
-            if (started) {
-                System.out.println("generating");
-                final TextView textGenerateNumber = (TextView)findViewById(R.id.text_temperature);
-                arry1.clear();
-                for(int i=10;i<11;i++){
-                    ArrayList<Integer> Arry1 = new ArrayList<Integer>();
-                    Arry1.add(myRandom.nextInt(25));
-                    arry1.add(Arry1);
-                    arry1.remove("[");
-                };
-
-                final TextView textwind = (TextView)findViewById(R.id.text_wind_spe);
-                arry2.clear();
-                for (double k=2;k<3;k++){
-                    ArrayList<Double> Arry2 = new ArrayList<Double>();
-                    Arry2.add(myRandom.nextDouble());
-                    arry2.add(Arry2);
+    Random Number;
 
 
+    int Rnumber;
+    double Dwind;
 
-                }
-                runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        textGenerateNumber.setText(String.valueOf(arry1));
-                        textwind.setText(String.valueOf(arry2));
-                    }
-                });
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,23 +36,124 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button buttonGenerate = (Button)findViewById(R.id.simpleSwitch);
+        TextView textView = (TextView) findViewById(R.id.text_temperature);
+        TextView textViewWind = (TextView) findViewById(R.id.text_wind_spe);
 
-        buttonGenerate.setOnClickListener(new View.OnClickListener(){
+        Thread t = new Thread() {
+
+
             @Override
-            public void onClick(View v) {
-                System.out.println("click");
-                if (!genTask.started) {
-                    genTask.started=true;
-                    timeoutTimer = new Timer();
-                    timeoutTimer.scheduleAtFixedRate(genTask, 0, 5000);
-                } else {
-                    genTask.started=false;
-                    timeoutTimer.cancel();
+            public void run() {
+
+                while (!isInterrupted()) {
+
+                    try {
+                        Thread.sleep(1000);  //1000ms = 1 sec
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Number = new Random();
+                                int Rnumber = ThreadLocalRandom.current().nextInt(10, 22);
+                                textView.setText(String.valueOf(Rnumber));
+
+                                double Dwind = ThreadLocalRandom.current().nextDouble(2, 3);
+                                textViewWind.setText(String.valueOf(Dwind));
+                            }
+                        });
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
                 }
             }
-        });
+        };
+
+        t.start();
+
     }
+
+
+
+
+
+
+
+//    Timer timeoutTimer;
+//    final Random myRandom = new Random();
+//    GenerateTask genTask = new GenerateTask();
+//    final ArrayList<ArrayList<Integer>> arry1 = new ArrayList<ArrayList<Integer>>();
+//    final ArrayList<ArrayList<Double>> arry2 = new ArrayList<ArrayList<Double>>();
+//
+//
+//
+//
+//
+//
+//
+//    class GenerateTask extends TimerTask {
+//        boolean started = false;
+//        @Override
+//        public void run() {
+//            if (started) {
+//                System.out.println("generating");
+//                final TextView textGenerateNumber = (TextView)findViewById(R.id.text_temperature);
+//                arry1.clear();
+//                for(int i=10;i<11;i++){
+//                    ArrayList<Integer> Arry1 = new ArrayList<Integer>();
+//                    Arry1.add(myRandom.nextInt(25));
+//                    arry1.add(Arry1);
+//                    arry1.remove("[");
+//                };
+//
+//                final TextView textwind = (TextView)findViewById(R.id.text_wind_spe);
+//                arry2.clear();
+//                for (double k=2;k<3;k++){
+//                    ArrayList<Double> Arry2 = new ArrayList<Double>();
+//                    Arry2.add(myRandom.nextDouble());
+//                    arry2.add(Arry2);
+//
+//
+//
+//                }
+//                runOnUiThread(new Runnable() {
+//
+//                    @Override
+//                    public void run() {
+//                        textGenerateNumber.setText(String.valueOf(arry1));
+//                        textwind.setText(String.valueOf(arry2));
+//                    }
+//                });
+//            }
+//        }
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        Button buttonGenerate = (Button)findViewById(R.id.simpleSwitch);
+//
+//        buttonGenerate.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("click");
+//                if (!genTask.started) {
+//                    genTask.started=true;
+//                    timeoutTimer = new Timer();
+//                    timeoutTimer.scheduleAtFixedRate(genTask, 0, 5000);
+//                } else {
+//                    genTask.started=false;
+//                    timeoutTimer.cancel();
+//                }
+//            }
+//        });
+//    }
 
 
 //    TextView temperature;
